79ea715a3d79bfb906963d6701e36ad955ce9c33
kurnia665 <79414192+kurnia665@users.noreply.github.com>
2021-03-01 16:03:49 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"GabutC-UBot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Dinda-CUTEbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
